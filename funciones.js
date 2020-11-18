b96fae219d8e463b2d03f307b41db7d6a3a88df6
RicardoBocanegra <ricardomendez145@gmail.com>
2020-11-17 23:21:36 -0600

    funcion

---
+++ funciones.js
@@ -0,0 +1,14 @@
+// Mensaje por consola
+console.log("Primera Práctica");
+// Declarando variables
+var variableVar = "Variable con Var";
+let variableLet = "Variable con Let";
+const variableConst = "Variable con Const";
+// Mostrando variables por consola.
+console.log(variableVar);
+console.log(variableLet);
+console.log(variableConst);
+// Repitiendo declaracion de variable var
+var variableVar = "Variable con Var";
+// Intentando asignar nuevo valor a variableConst
+variableConst = "Nuevo Valor de Variable Const";
